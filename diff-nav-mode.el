9fc9c4c8d4172635ecaa75fe45ec3b5bc2085251
Matt McClure <matt.mcclure@mapmyfitness.com>
2012-02-20 20:30:51 -0500

    Made ediff work from diff buffer on new/nonexistent pairs.

--- diff-nav-mode.el
+++ diff-nav-mode.el
@@ -21,11 +21,8 @@
 (defun diff-nav-do-ediff ()
   (interactive)
   (message "called diff-nav-do-ediff")
-  (let* ((dirs (parse-diff-command-line
-                (first-line)))
-         (files (parse-file-summary-line
-                 (buffer-substring (line-beginning-position) (line-end-position)) dirs)))
-    (ediff-files (car files) (cadr files)))
+  (let ((files (diff-hunk-file-names)))
+    (ediff-buffers (get-file-buffer (cadr files)) (get-file-buffer (car files))))
   )
 
 (defun parse-diff-command-line (line)
@@ -66,3 +63,52 @@
     )
    )
   )
+
+(defun ediff-find-file (file-var buffer-name &optional last-dir hooks-var)
+  "Visit FILE and arrange its buffer to Ediff's liking.
+FILE-VAR is actually a variable symbol whose value must contain a true
+file name.
+BUFFER-NAME is a variable symbol, which will get the buffer object into
+which FILE is read.
+LAST-DIR is the directory variable symbol where FILE's
+directory name should be returned.  HOOKS-VAR is a variable symbol that will
+be assigned the hook to be executed after `ediff-startup' is finished.
+`ediff-find-file' arranges that the temp files it might create will be
+deleted."
+  (let* ((file (symbol-value file-var))
+	 (file-magic (ediff-filename-magic-p file))
+	 (temp-file-name-prefix (file-name-nondirectory file)))
+    (cond ((file-directory-p file)
+	   (error "File `%s' is a directory" file)))
+
+    ;; some of the commands, below, require full file name
+    (setq file (expand-file-name file))
+
+    ;; Record the directory of the file
+    (if last-dir
+	(set last-dir (expand-file-name (file-name-directory file))))
+
+    ;; Setup the buffer
+    (set buffer-name (find-file-noselect file))
+
+    (ediff-with-current-buffer (symbol-value buffer-name)
+      (widen) ; Make sure the entire file is seen
+      (cond (file-magic  ;   file has a handler, such as jka-compr-handler or
+	     		 ;;; ange-ftp-hook-function--arrange for temp file
+	     (ediff-verify-file-buffer 'magic)
+	     (setq file
+		   (ediff-make-temp-file
+		    (current-buffer) temp-file-name-prefix))
+	     (set hooks-var (cons `(lambda () (delete-file ,file))
+				  (symbol-value hooks-var))))
+	    ;; file processed via auto-mode-alist, a la uncompress.el
+	    ((not (equal (file-truename file)
+			 (file-truename (buffer-file-name))))
+	     (setq file
+		   (ediff-make-temp-file
+		    (current-buffer) temp-file-name-prefix))
+	     (set hooks-var (cons `(lambda () (delete-file ,file))
+				  (symbol-value hooks-var))))
+	    (t ;; plain file---just check that the file matches the buffer
+	     (ediff-verify-file-buffer))))
+    (set file-var file)))
