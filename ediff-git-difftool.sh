aec967a98465591ffbd13e42e3f40656923e7339
Matt McClure <matt.mcclure@mapmyfitness.com>
2012-02-20 22:42:54 -0500

    Sets up the Emacs frame with the *Diff* buffer in the only window, in read-only mode.

--- ediff-git-difftool.sh
+++ ediff-git-difftool.sh
@@ -3,4 +3,4 @@
 LOCAL="$1"
 REMOTE="$2"
 
-emacs --eval="(diff \"$LOCAL\" \"$REMOTE\" \"-urN\")"
+emacs --eval="(progn (diff \"$LOCAL\" \"$REMOTE\" \"-urN\") (switch-to-buffer \"*Diff*\") (diff-nav-mode) (delete-other-windows) (beginning-of-buffer) (toggle-read-only t))"
