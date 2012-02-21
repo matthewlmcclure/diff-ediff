ada68b3ae209735bdc65867085a2af280cda221b
Matt McClure <matt.mcclure@mapmyfitness.com>
2012-02-20 23:27:07 -0500

    Renames diff-nav-mode as diff-ediff-mode.

--- ediff-git-difftool.sh
+++ ediff-git-difftool.sh
@@ -3,4 +3,4 @@
 LOCAL="$1"
 REMOTE="$2"
 
-emacs --eval="(progn (diff \"$LOCAL\" \"$REMOTE\" \"-urN\") (switch-to-buffer \"*Diff*\") (diff-nav-mode) (delete-other-windows) (beginning-of-buffer) (toggle-read-only t))"
+emacs --eval="(progn (diff \"$LOCAL\" \"$REMOTE\" \"-urN\") (switch-to-buffer \"*Diff*\") (diff-ediff-mode) (delete-other-windows) (beginning-of-buffer) (toggle-read-only t))"
