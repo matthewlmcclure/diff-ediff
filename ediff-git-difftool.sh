fd2e0aadfa12cb8e810c033976be44d78cf9502f
Matt McClure <matt.mcclure@mapmyfitness.com>
2012-02-22 14:53:12 -0500

    Uses an interactive bash shell as Emacs's parent process to work around a defect that otherwise makes C-g exit git commit.

--- ediff-git-difftool.sh
+++ ediff-git-difftool.sh
@@ -1,6 +1,7 @@
-#! /bin/bash
+#! /bin/bash -i
 
 LOCAL="$1"
 REMOTE="$2"
+
 
 emacs --eval="(progn (diff \"$LOCAL\" \"$REMOTE\" \"-urN\") (switch-to-buffer \"*Diff*\") (diff-ediff-mode) (delete-other-windows) (beginning-of-buffer) (toggle-read-only t))"
